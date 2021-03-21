86c5fc802ab86ed2ae21fcd5f568f03c941bd1b6
German Fomenko <pentagram777@mail.ru>
2021-03-21 18:03:03 +0500

    add animation for furniture item

--- src/components/FurnitureItems.jsx
+++ src/components/FurnitureItems.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import Pulse from "react-reveal/Pulse";
 
 function FurnitureItems({ title, img }) {
 
   return (
+    <Pulse>
+
     <div className="furniture__item" >
       <div className="furniture__item-wrapper">
         <div className="furniture__item-img">
@@ -12,6 +15,7 @@ function FurnitureItems({ title, img }) {
         <div className="furniture__item-title">{title}</div>
       </div>
     </div>
+    </Pulse>
   );
 }
 
