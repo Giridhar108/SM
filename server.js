f93062e7fbf0f6f633d14134b52f01e52cf62b4d
root <root@482719-cy96588.tmweb.ru>
2021-04-03 05:30:07 +0300

    feat ssl

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 const path = require('path')
+const app = express();
 
-const PORT = process.env.PORT || 80
+// Certificate
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/spiritmebel.ru/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/spiritmebel.ru/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/spiritmebel.ru/chain.pem', 'utf8');
 
-const app = express()
+const credentials = {
+key: privateKey,
+cert: certificate,
+ca: ca
+};
 app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
@@ -11,4 +22,14 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
-app.listen(PORT)
+// Starting both http & https servers
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+console.log('HTTP Server running on port 80');
+});
+
+httpsServer.listen(443, () => {
+console.log('HTTPS Server running on port 443');
+});
